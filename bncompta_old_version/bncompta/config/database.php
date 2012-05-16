21906057c5e0fff3e9357ae3a6db57ea8fdf3887
Karim Besbes <contact@karimbesbes.com>
2012-05-15 23:05:10 -0300

    README

--- bncompta_old_version/bncompta/config/database.php
+++
@@ -1,8 +0,0 @@
-Logiciel libre de comptabilité pour les indépendants, facile à utiliser basé sur le modèle de recettes/dépenses.
-
-PLEASE NOTE : 
-
-BNCOMPTA is under heavy development, a lot of efforts is being made to rewrite the entire code of the old version. 
-
-
- 
